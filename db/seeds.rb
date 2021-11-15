ba8a8d8cbef95b8aac598bc4c1812337d4bd5bfe
CollinHU <humy720@163.com>
2021-11-14 19:17:35 -0800

    in db/seeds.rd generate fake users, vlogs, comments, likes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+for i in 1..5 do 
+    user=User.new(
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.password
+  )
+  user.save!
+end
+
+for i in 1..10 do
+	Vlog.create(
+		title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
+		content: Faker::Lorem.paragraph(sentence_count: 6, supplemental: true, random_sentences_to_add: 4),
+    	user_id: rand(1..5)
+    )
+  
+end
+
+for i in 1..30 do
+	Comment.create(
+		comment: Faker::Lorem.sentence(word_count: 10, supplemental: false, random_words_to_add: 10),
+    	vlog_id: rand(1..6),
+    	user_id: rand(1..5),
+		)
+end
+
+for i in 1..30 do
+	Like.create(
+    	vlog_id: rand(1..10),
+    	user_id: rand(1..5),
+		)
+end
