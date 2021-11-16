8f5db601fff09bf23a4d6d0a3ca138b62e90c874
CollinHU <humy720@163.com>
2021-11-16 09:08:29 -0800

    gen 1K users, 10K vlogs,...

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-for i in 1..5 do 
+for i in 1..1000 do 
 	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
     user=User.new(
     email: Faker::Internet.unique.email,
@@ -18,34 +18,34 @@ for i in 1..5 do
   user.save!
 end
 
-for i in 1..10 do
+for i in 1..10000 do
 	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
 	Vlog.create(
 		title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
 		content: Faker::Lorem.paragraph(sentence_count: 6, supplemental: true, random_sentences_to_add: 4),
 		created_at: date,
 		updated_at: date,
-    	user_id: rand(1..5)
+    	user_id: rand(1..1000)
     )
   
 end
 
-for i in 1..30 do
+for i in 1..30000 do
 	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
 	Comment.create(
 		comment: Faker::Lorem.sentence(word_count: 20, supplemental: false, random_words_to_add: 10),
-    	vlog_id: rand(1..6),
-    	user_id: rand(1..5),
+    	vlog_id: rand(1..10000),
+    	user_id: rand(1..1000),
     	created_at: date,
 		updated_at: date,
 		)
 end
 
-for i in 1..30 do
+for i in 1..40000 do
 	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
 	Like.create(
-    	vlog_id: rand(1..10),
-    	user_id: rand(1..5),
+    	vlog_id: rand(1..10000),
+    	user_id: rand(1..1000),
     	created_at: date,
 		updated_at: date,
 		)
