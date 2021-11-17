82c57498c7fc8d2b27c0ace969cf8f862d23fb64
CollinHU <humy720@163.com>
2021-11-16 18:46:42 -0800

    create db by import csv file:10K users, 10K vlogs, 100K comments, 100K likes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,48 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+#require 'faker'
 
-for i in 1..1000 do 
-	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
-    user=User.new(
-    email: Faker::Internet.unique.email,
-    password: Faker::Internet.password,
-    created_at: date,
-	updated_at: date
-  )
-  user.save!
+#for i in 1..100 do 
+#	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
+#    User.create!(
+#    email: Faker::Internet.unique.email,
+#    password: "1234567",
+#    created_at: date,
+#	updated_at: date
+#  )
+#  #user.save!
+#end
+
+require 'csv'
+ 
+User.transaction do
+   users = CSV.read("#{Rails.root}/FakeDataGenerator/users.csv")
+   columns = [:email, :encrypted_password, :created_at, :updated_at]
+   User.import columns, users, validate: false
 end
 
-for i in 1..10000 do
-	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
-	Vlog.create(
-		title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
-		content: Faker::Lorem.paragraph(sentence_count: 6, supplemental: true, random_sentences_to_add: 4),
-		created_at: date,
-		updated_at: date,
-    	user_id: rand(1..1000)
-    )
-  
+
+
+Vlog.transaction do
+   users = CSV.read("#{Rails.root}/FakeDataGenerator/vlogs.csv")
+   columns = [:title, :content, :created_at, :updated_at, :user_id]
+   Vlog.import columns, users, validate: false
 end
 
-for i in 1..30000 do
-	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
-	Comment.create(
-		comment: Faker::Lorem.sentence(word_count: 20, supplemental: false, random_words_to_add: 10),
-    	vlog_id: rand(1..10000),
-    	user_id: rand(1..1000),
-    	created_at: date,
-		updated_at: date,
-		)
+#for i in 1..10 do
+#	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
+#	Vlog.create(
+#		title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
+#		content: Faker::Lorem.paragraph(sentence_count: 6, supplemental: true, random_sentences_to_add: 4),
+#		created_at: date,
+#		updated_at: date,
+#    	user_id: rand(1..100)
+#    )
+#  
+#end
+
+Comment.transaction do
+   users = CSV.read("#{Rails.root}/FakeDataGenerator/comments.csv")
+   columns = [:comment, :vlog_id, :user_id, :created_at, :updated_at]
+   Comment.import columns, users, validate: false
+end
+#for i in 1..100 do
+#	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
+#	Comment.create(
+#		comment: Faker::Lorem.sentence(word_count: 20, supplemental: false, random_words_to_add: 10),
+#    	vlog_id: rand(1..100),
+#    	user_id: rand(1..100),
+#    	created_at: date,
+#		updated_at: date,
+#		)
+#end
+
+#for i in 1..100 do
+#	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
+#	Like.create(
+#    	vlog_id: rand(1..100),
+#    	user_id: rand(1..100),
+#    	created_at: date,
+#		updated_at: date,
+#		)
+#end
+
+Like.transaction do
+   users = CSV.read("#{Rails.root}/FakeDataGenerator/likes.csv")
+   columns = [:vlog_id, :user_id, :created_at, :updated_at]
+   Like.import columns, users, validate: false
 end
 
-for i in 1..40000 do
-	date = Faker::Date.between(from: '2010-09-23', to: '2021-09-25')
-	Like.create(
-    	vlog_id: rand(1..10000),
-    	user_id: rand(1..1000),
-    	created_at: date,
-		updated_at: date,
-		)
-end
+
