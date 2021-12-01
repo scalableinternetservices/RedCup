96f946772c6cdc9a815c1fbe19a5064f81814db1
CollinHU <humy720@163.com>
2021-11-30 17:33:24 -0800

    disable authentication

--- app/models/vlog.rb
+++ app/models/vlog.rb
@@ -2,6 +2,6 @@ class Vlog < ApplicationRecord
 	belongs_to :user
 	has_many :comments
 	has_many :likes
-	validates :title, :content, presence: true
+	#validates :title, :content, presence: true
 	has_one_attached :file
 end
